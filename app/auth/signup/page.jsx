2421faeca1a25e1dc6286ac29dda16542d3a61f7
Milton <ajaxmilton@hotmail.com>
2025-10-12 19:15:36 +0200

    Add signup page with form fields and social sign-in options

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -1,7 +1,75 @@
 import React from "react";
+import Link from "next/link";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-col max-w-md p-4 mx-auto mt-10 rounded-lg shadow-lg">
+      <div>
+        <h1 className="text-2xl font-bold text-gray-900 text-center ">BS</h1>
+        <h2 className="text-xl text-gray-900 text-center my-6">
+          Log in into your BS account
+        </h2>
+      </div>
+      <form action="">
+        <label htmlFor="" className="flex flex-col mb-2">
+          <span className="text-gray-600 p-1">Username</span>
+          <input
+            type=""
+            className="p-2 text-s text-gray-600 rounded-md border border-gray-300 bg-gray-100 outline-gray-400"
+            required
+          />
+        </label>
+        <label htmlFor="" className="flex flex-col mb-2">
+          <span className="text-gray-600 p-1">Email</span>
+          <input
+            type="email"
+            className="p-2 text-s text-gray-600 rounded-md border border-gray-300 bg-gray-100 outline-gray-400"
+            required
+          />
+        </label>
+        <label htmlFor="" className="flex flex-col mb-4">
+          <span className="text-gray-600 p-1">Password</span>
+          <input
+            type="password"
+            className="p-2 text-s text-gray-600 rounded-md border border-gray-300 bg-gray-100 outline-gray-400"
+            required
+          />
+        </label>
+        <label htmlFor="" className="flex flex-col mb-4">
+          <span className="text-gray-600 p-1">Confirm password</span>
+          <input
+            type="password"
+            className="p-2 text-s text-gray-600 rounded-md border border-gray-300 bg-gray-100 outline-gray-400"
+            required
+          />
+        </label>
+        <button
+          type="submit"
+          className="bg-gray-900 text-white w-[100%] p-2 rounded-md hover:bg-gray-800 mb-4 hover:cursor-pointer"
+        >
+          Log In
+        </button>
+      </form>
+      <p className="text-center font-bold text-slate-600">OR</p>
+      <div>
+        <button className="border border-gray-600 text-gray-900 w-[100%] p-2 rounded-md hover:bg-gray-800 mb-4 hover:cursor-pointer hover:text-white">
+          Sign Up in with Google
+        </button>
+        <button className="border border-gray-600 text-gray-900 w-[100%] p-2 rounded-md hover:bg-gray-800 mb-4 hover:cursor-pointer hover:text-white">
+          Sign Up with Facebook
+        </button>
+        <button className="border border-gray-600 text-gray-900 w-[100%] p-2 rounded-md hover:bg-gray-800 mb-4 hover:cursor-pointer hover:text-white">
+          Sign Up with Apple
+        </button>
+      </div>
+      <Link
+        href={"/auth/login"}
+        className="text-center text-slate-500 text-[0.8rem] hover:underline hover:cursor-pointer"
+      >
+        Already have an account? Log In
+      </Link>
+    </div>
+  );
 };
 
 export default page;
