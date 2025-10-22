af457b37df18e615f8293862162747314a7937d1
Milton <ajaxmilton@hotmail.com>
2025-10-22 21:30:25 +0200

    Refine electives hover styles

--- components/CourseCard.jsx
+++ components/CourseCard.jsx
@@ -2,9 +2,15 @@
 
 import React from "react";
 
-const CourseCard = ({ course }) => {
+const CourseCard = ({ course, heading }) => {
   return (
-    <div className="p-4 border flex flex-col justify-around items-start border-gray-400 rounded-2xl h-[230px]">
+    <div
+      className={
+        heading === "Electives"
+          ? "p-4 border flex flex-col justify-around items-start border-amber-400 rounded-2xl h-[230px]"
+          : "p-4 border flex flex-col justify-around items-start border-gray-400 rounded-2xl h-[230px]"
+      }
+    >
       <div>
         <h2 className="text-2xl font-semibold text-gray-900">{course.title}</h2>
         <h3 className="text-[0.85rem] text-gray-700">
@@ -13,10 +19,18 @@ const CourseCard = ({ course }) => {
         </h3>
       </div>
 
-      <p className="text-[0.9rem] text-gray-700 my-2 line-clamp-4">{course.desc}</p>
-      <button className="px-5 py-[6px] rounded-lg bg-gray-900 text-[0.85rem] text-white hover:cursor-pointer hover:bg-gray-800">
-        View Modules
-      </button>
+      <p className="text-[0.9rem] text-gray-700 my-2 line-clamp-4">
+        {course.desc}
+      </p>
+      {heading === "Electives" ? (
+        <button className="px-5 py-[6px] border border-transparent rounded-lg bg-yellow-500 text-[0.85rem] text-gray-900 font-bold hover:cursor-pointer hover:border hover:border-amber-500 hover:bg-transparent hover:text-gray-900">
+          View Modules
+        </button>
+      ) : (
+        <button className="px-5 py-[6px] rounded-lg bg-gray-900 text-[0.85rem] text-white hover:cursor-pointer hover:bg-gray-800">
+          View Modules
+        </button>
+      )}
     </div>
   );
 };
--- components/Jumbotron.jsx
+++ components/Jumbotron.jsx
@@ -6,21 +6,19 @@ const Jumbotron = () => {
     <div className="h-[70vh] grid grid-cols-2 bg-gray-400 w-[100%] p-5 mb-20">
       <div className="col-start-1 col-end-2 px-10 flex flex-col items-start justify-center">
         <div className="mb-10">
-          <h2 className="text-7xl font-bold">Learn Theology </h2>
-          <h2 className="font-light text-5xl">at your own pace</h2>
+          <h2 className="text-7xl text-gray-900 font-bold">Learn Theology </h2>
+          <h2 className="font-light text-gray-200 text-5xl">at your own pace</h2>
           <p className="font-light text-lg mt-5">
             Explore deep theological concepts and biblical studies through our
             flexible online courses designed to fit your schedule and learning
             style
           </p>
         </div>
-        <button className="bg-black text-white px-8 py-2 rounded-lg">
+        <button className="bg-gray-900 text-white px-8 py-2 rounded-lg hover:bg-gray-800 hover:cursor-pointer">
           Start Now
         </button>
       </div>
-      <div className="col-start-2 col-end-3 flex items-center justify-center rounded-sm">
-        
-      </div>
+      <div className="col-start-2 col-end-3 flex items-center justify-center rounded-sm"></div>
     </div>
   );
 };
